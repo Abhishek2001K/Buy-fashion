7642cdad91549c1c548461374c677ed67db056f3
Abhishek2001K <127178414+Abhishek2001K@users.noreply.github.com>
2024-07-29 14:17:53 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
-
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Shop from './Pages/Shop';
 import ShopCategory from './Pages/ShopCategory';
 import Product from './Pages/Product';
@@ -14,23 +13,20 @@ import kid_banner from './Components/Assets/banner_kids.png'
 
 function App() {
   return (
-    <div>
-      <BrowserRouter>
-      <Navbar/>
+    <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route index element={<Shop />} />
-        <Route path='/mens' element={<ShopCategory banner={men_banner} category="men"/>}/>
-        <Route path='/womens' element={<ShopCategory banner={women_banner} category="women"/>}/>
-        <Route path='/kids' element={<ShopCategory banner={kid_banner} category="kid"/>}/>
-        <Route path="/product" element={<Product/>}>
-          <Route path=':productId' element={<Product/>}/>
-        </Route>
-        <Route path='/cart' element={<Cart/>}/>
-        <Route path='/login' element={<LoginSignup/>}/>
+        <Route path="/mens" element={<ShopCategory banner={men_banner} category="men" />} />
+        <Route path="/womens" element={<ShopCategory banner={women_banner} category="women" />} />
+        <Route path="/kids" element={<ShopCategory banner={kid_banner} category="kid" />} />
+        <Route path="/product/:productId" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<LoginSignup />} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
-      <Footer/>
-      </BrowserRouter>
-    </div>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
